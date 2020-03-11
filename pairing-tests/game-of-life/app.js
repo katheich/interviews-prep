ee7f54f075e9a3f7cf4a6fc02c9f2a6647c55e91
Kathrin Eichinger <kathrin801@gmail.com>
2020-03-11 11:42:45 +0000

    game of life - state assessment loop

--- pairing-tests/game-of-life/app.js
+++ pairing-tests/game-of-life/app.js
@@ -1,9 +1,10 @@
-// params
+// params and vars
 const size = 10
+let generation = 0
 
 // make the grid - array of arrays
 
-const gridArray = []
+let gridArray = []
 
 for (let y = 0; y < size; y++) {
   const row = []
@@ -25,6 +26,7 @@ gridArray[7][8] = 'x'
 gridArray[1][9] = 'x'
 gridArray[4][5] = 'x'
 gridArray[5][6] = 'x'
+gridArray[2][2] = 'o'
 
 // visualise grid
 function visualGrid(grid) {
@@ -47,7 +49,7 @@ function getNeighbourCells(x, y) {
   return [[minX, minY], [x, minY], [maxX, minY], [minX, y], [maxX, y], [minX, maxY], [x, maxY], [maxX, maxY]]
 }
 
-// getNeighbourCells(0, 3).forEach(coord => {
+// getNeighbourCells(2, 2).forEach(coord => {
 //   console.log(coord)
 //   gridArray[coord[1]][coord[0]] = 'o'
 // })
@@ -66,31 +68,67 @@ function stateAssessment(x, y) {
     return alive += gridArray[coord[1]][coord[0]] === 'x' ? 1 : 0
   }, 0)
 
-  if (gridArray[x][y] === 'x') {
+  // console.log('alive: ', alive)
+
+  if (gridArray[y][x] === 'x') {
 
     if (alive < 2) {
+      // console.log('starve')
       return '.'
 
     } else if (alive < 4) {
+      // console.log('fine')
       return 'x'
 
     } else {
+      // console.log('overpopulated')
       return '.'
     }
 
   } else {
 
     if (alive === 3) {
+      // console.log('come alive')
       return 'x'
 
     } else {
+      // console.log('still dead')
       return '.'
     }
   }
 }
 
-console.log(stateAssessment(2, 3))
-
-
 
 ///// run state-assessment function repeatedly at fixed interval
+
+const interval = setInterval(() => {
+  generation++
+  const newGrid = gridArray.map(row => [...row])
+  // console.log(newGrid)
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+      // console.log(x, y)
+      // console.log(newGrid[y][x])
+      // console.log(stateAssessment(x, y))
+      newGrid[y][x] = stateAssessment(x, y)
+    }
+  }
+  checkChanges(newGrid, gridArray)
+  gridArray = newGrid
+  console.log(`Generation ${generation}`)
+  visualGrid(gridArray)
+}, 3000)
+
+
+function checkChanges(newGrid, prevGrid) {
+  let same = true
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+      newGrid[y][x] === prevGrid[y][x] ? same = same && true : same = false
+    }
+  }
+
+  if (same) {
+    clearInterval(interval)
+  }
+}
